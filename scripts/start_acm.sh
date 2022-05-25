d0da2d71ad799e2e5f088f121ed9fa8de4571bd0
kilometers <kilo@meters.sh>
2022-05-24 22:46:04 -0700

    Fix docker command

--- scripts/start_acm.sh
+++ scripts/start_acm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-server --dist-path=/usr/share/acm/dist --database-url=/acm/db.sqlite &
+server --dist-path=/usr/share/acm/dist --database-url=/acm/db.sqlite --port=80 --hostname=0.0.0.0 &
 ramiel &
 
 wait -n
