76c8687ec4f9114c275fc2788a0fe3e00ae0ba7b
kilometers <kilo@meters.sh>
2022-06-05 02:50:21 -0700

    Improve static rendering

--- lilith/tailwind.config.js
+++ lilith/tailwind.config.js
@@ -13,6 +13,16 @@ module.exports = {
         'full': 'minmax(0,1fr)',
         'min-full': 'min-content minmax(0,1fr)'
       },
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '50%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        }
+      },
+      animation: {
+        'fade-in': 'fade-in 1s ease-in-out'
+      },
       typography: theme => ({
         neutral: {
           css: {
