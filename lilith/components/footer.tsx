a65623922b8e42f6409662f9a740b6004ac15984
kilometers <kilo@meters.sh>
2022-08-30 22:14:49 -0700

    Remove discord

--- lilith/components/footer.tsx
+++ lilith/components/footer.tsx
@@ -2,8 +2,6 @@ export default function Footer(): JSX.Element {
   return (
     <div className="h-52 w-full bg-neutral-100 border-neutral-300 dark:border-neutral-600 border-t flex items-center justify-center dark:bg-neutral-800 mt-auto">
       <div className="flex flex-col gap-2 justify-center">
-      <span className="select-none">Check out our <a className="text-blue-500 hover:text-blue-700 hover:underline transition-colors" href="https://discord.gg/ZNUxzVketd">Discord</a> server!</span>
-
       <span className="select-none text-center">made with 🐌</span>
       </div>
     </div>
