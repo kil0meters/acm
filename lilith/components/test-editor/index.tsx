954752e4f766388dca5c9bb426dadd69612ac4a8
kilometers <kilo@meters.sh>
2023-03-24 10:48:01 -0700

    Update test editors some more

--- lilith/components/test-editor/index.tsx
+++ lilith/components/test-editor/index.tsx
@@ -6,11 +6,12 @@ import { FunctionValue } from "../problem/submission/tests";
 // Give me rust enums please
 type GridEditorProps = {
     data: any[][],
+    type: string,
 
     onChange: (grid: any[][]) => void,
 };
 
-function GridEditor({ data, onChange }: GridEditorProps) {
+function GridEditor({ type, data, onChange }: GridEditorProps) {
     const [gridString, setGridString] = useState(JSON.stringify(data));
     const [isValid, setIsValid] = useState(false);
 
@@ -24,10 +25,11 @@ function GridEditor({ data, onChange }: GridEditorProps) {
             setIsValid(false);
         }
 
-    }, [gridString]);
+    }, [gridString, onChange]);
 
     return (
         <div>
+            <span className="text-sm">{type} Grid:</span>
             <textarea value={gridString} className={`p-2 outline w-full font-mono focus:outline-4 rounded-md ${isValid ? "outline-green-500" : "outline-red-500"}`} onChange={(e) => {
                 setGridString(e.currentTarget.value);
             }} />
@@ -37,130 +39,64 @@ function GridEditor({ data, onChange }: GridEditorProps) {
 
 type GraphEditorProps = {
     data: any[][],
+    type: string,
 
     onChange: (grid: any[][]) => void,
 };
 
-function GraphEditor({ data, onChange }: GraphEditorProps) {
-    let [graph, setGraph] = useState(data);
+function GraphEditor({ type, data, onChange }: GraphEditorProps) {
+    const [graphString, setGraphString] = useState(JSON.stringify(data));
+    const [isValid, setIsValid] = useState(false);
 
     useEffect(() => {
-        onChange(graph);
-    }, [graph, onChange]);
-
-    const addRow = () => {
-        setGraph(produce(graph, newGraph => {
-            newGraph.push(Array(graph[0].length).fill(""));
-        }));
-    };
-
-    const removeRow = () => {
-        if (graph.length > 1) {
-            setGraph(produce(graph, newGraph => {
-                newGraph.pop();
-            }));
+        try {
+            let graph = JSON.parse(graphString);
+            setIsValid(true);
+            onChange(graph);
         }
-    };
-
-    const addCol = (row: number) => {
-        setGraph(produce(graph, newGraph => {
-            newGraph[row].push("");
-        }));
-    };
-
-    const removeCol = (row: number) => {
-        if (graph[row].length > 1) {
-            setGraph(produce(graph, newGraph => {
-                newGraph[row].pop();
-            }));
+        catch (_e) {
+            setIsValid(false);
         }
-    };
+
+    }, [graphString, onChange]);
 
     return (
-        <div className="flex flex-col gap-2">
-            <div>
-                {
-                    graph.map((row, y) =>
-                        <div key={y} className="flex">
-                            <span className="my-auto mr-2 font-mono">{y}:</span>
-                            <div className="border flex w-full border-blue-200 dark:border-slate-700">
-                                {row.map((element, x) =>
-                                    <input
-                                        key={x}
-                                        className="border p-2 w-full bg-blue-50 dark:bg-slate-800 border-blue-200 dark:border-slate-700"
-                                        type="text"
-                                        value={element.toString()}
-                                        onChange={(e) => {
-                                            setGraph(produce(graph, newGraph => {
-                                                try {
-                                                    newGraph[y][x] = JSON.parse(e.target.value);
-                                                } catch {
-                                                    newGraph[y][x] = e.target.value as any;
-                                                }
-                                            }));
-                                        }}
-                                    />
-                                )}
-                            </div>
-
-                            <div className="flex mx-auto my-auto gap-2 ml-2">
-                                <button
-                                    onClick={() => addCol(y)}
-                                    className="bg-neutral-200 hover:bg-neutral-300 transition-colors rounded-full h-6 px-4 dark:bg-neutral-700 dark:hover:bg-neutral-600"
-                                >+</button>
-                                <button
-                                    onClick={() => removeCol(y)}
-                                    className="bg-neutral-200 hover:bg-neutral-300 transition-colors rounded-full h-6 px-4 dark:bg-neutral-700 dark:hover:bg-neutral-600"
-                                >-</button>
-                            </div>
-                        </div>
-                    )
-                }
-            </div>
-
-            <div className="flex mx-auto my-auto gap-4">
-                <button
-                    onClick={addRow}
-                    className="bg-neutral-200 hover:bg-neutral-300 transition-colors rounded-full h-6 px-4 dark:bg-neutral-700 dark:hover:bg-neutral-600"
-                >+</button>
-                <button
-                    onClick={removeRow}
-                    className="bg-neutral-200 hover:bg-neutral-300 transition-colors rounded-full h-6 px-4 dark:bg-neutral-700 dark:hover:bg-neutral-600"
-                >-</button>
-            </div>
+        <div>
+            <span className="text-sm">{type} Graph (adjacency list):</span>
+            <textarea value={graphString} className={`p-2 outline w-full font-mono focus:outline-4 rounded-md ${isValid ? "outline-green-500" : "outline-red-500"}`} onChange={(e) => {
+                setGraphString(e.currentTarget.value);
+            }} />
         </div>
     );
 }
 
 type ListEditorProps = {
     data: any[],
+    type: string,
 
     onChange: (list: any[]) => void,
 };
 
-function ListEditor({ data, onChange }: ListEditorProps) {
-    const [list, setList] = useState(data);
-    const [listString, setListString] = useState("[]");
+function ListEditor({ type, data, onChange }: ListEditorProps) {
+    const [listString, setListString] = useState(JSON.stringify(data));
     const [isValid, setIsValid] = useState(false);
 
     useEffect(() => {
-        onChange(list);
-    }, [list, onChange]);
-
-    useEffect(() => {
         try {
-            setList(JSON.parse(listString));
+            let list = JSON.parse(listString);
             setIsValid(true);
+            onChange(list);
         }
         catch (_e) {
             setIsValid(false);
         }
 
-    }, [listString]);
+    }, [listString, onChange]);
 
     return (
         <div>
-            <input value={listString} className={`outline p-2 focus:outline-4 font-mono rounded-md ${isValid ? "outline-green-500" : "outline-red-500"}`} onChange={(e) => {
+            <span className="text-sm">{type} List:</span>
+            <input value={listString} className={`w-full outline p-2 focus:outline-4 font-mono rounded-md ${isValid ? "outline-green-500" : "outline-red-500"}`} onChange={(e) => {
                 setListString(e.currentTarget.value);
             }} />
         </div>
@@ -169,11 +105,12 @@ function ListEditor({ data, onChange }: ListEditorProps) {
 
 type SingleEditorProps = {
     value: any,
+    type: string,
 
     onChange: (grid: any) => void,
 }
 
-function SingleEditor({ value, onChange }: SingleEditorProps) {
+function SingleEditor({ type, value, onChange }: SingleEditorProps) {
     let [data, setData] = useState(value);
 
     useEffect(() => {
@@ -181,16 +118,19 @@ function SingleEditor({ value, onChange }: SingleEditorProps) {
     }, [data, onChange]);
 
     return (
-        <input
-            className="outline focus:outline-4 p-2 w-full rounded-md outline-green-500"
-            value={data.toString()}
-            onChange={(e) => {
-                try {
-                    setData(JSON.parse(e.target.value));
-                } catch {
-                    setData(e.target.value);
-                }
-            }} />
+        <div>
+            <span className="text-sm">{type}:</span>
+            <input
+                className="outline focus:outline-4 p-2 w-full rounded-md outline-green-500"
+                value={data.toString()}
+                onChange={(e) => {
+                    try {
+                        setData(JSON.parse(e.target.value));
+                    } catch {
+                        setData(e.target.value);
+                    }
+                }} />
+        </div>
     );
 }
 
@@ -205,25 +145,25 @@ function FunctionArgumentEditor({ arg, onChange }: FunctionArgumentEditorProps) 
 
     if ("Grid" in variant) {
         return (
-            <GridEditor data={variant.Grid} onChange={(grid) => {
+            <GridEditor type={type} data={variant.Grid} onChange={(grid) => {
                 // typescript: trust me bro
                 onChange({ [type]: { Grid: grid } } as FunctionValue);
             }} />
         );
     } else if ("Graph" in variant) {
         return (
-            <GraphEditor data={variant.Graph} onChange={(graph) => {
+            <GraphEditor type={type} data={variant.Graph} onChange={(graph) => {
                 onChange({ [type]: { Graph: graph } } as FunctionValue);
             }} />
         );
     } else if ("List" in variant) {
         return (
-            <ListEditor data={variant.List} onChange={(list) => {
+            <ListEditor type={type} data={variant.List} onChange={(list) => {
                 onChange({ [type]: { List: list } } as FunctionValue);
             }} />
         );
     } else if ("Single" in variant) {
-        return <SingleEditor value={variant.Single} onChange={(single) => {
+        return <SingleEditor type={type} value={variant.Single} onChange={(single) => {
             onChange({ [type]: { Single: single } } as FunctionValue);
         }} />
     }
