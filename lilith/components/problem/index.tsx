01028f8738a09593446e9b273cc4e0f1584f89db
kilometers <kilo@meters.sh>
2023-04-25 16:47:59 -0700

    Sticky sidebar on submissions page

--- lilith/components/problem/index.tsx
+++ lilith/components/problem/index.tsx
@@ -19,98 +19,98 @@ export const ProblemContext = createContext<Problem | undefined>(undefined);
 export const ProblemIDContext = createContext<number | undefined>(undefined);
 
 type ProblemViewProps = {
-  id?: number,
-  competitionId?: number
+    id?: number,
+    competitionId?: number
 };
 
 type Problem = {
-  id: number;
-  title: string;
-  description: string;
-  template: string;
-  competition_id?: number;
+    id: number;
+    title: string;
+    description: string;
+    template: string;
+    competition_id?: number;
 };
 
 export default function ProblemView({ id }: ProblemViewProps): JSX.Element {
-  const { data, error } = useSWR<Problem>(
-    id ? api_url(`/problems/${id}`) : null,
-    fetcher
-  );
+    const { data, error } = useSWR<Problem>(
+        id ? api_url(`/problems/${id}`) : null,
+        fetcher
+    );
 
-  function ProblemEditorWrapper(): JSX.Element {
-    // bad
-    let content =
-      useStore((state) =>
-        id ? state.problemImpls[id] : undefined
-      ) ??
-      data?.template ??
-      "";
+    function ProblemEditorWrapper(): JSX.Element {
+        // bad
+        let content =
+            useStore((state) =>
+                id ? state.problemImpls[id] : undefined
+            ) ??
+            data?.template ??
+            "";
 
-    const setProblemImpl = useStore((state) => state.setProblemImpl);
+        const setProblemImpl = useStore((state) => state.setProblemImpl);
+
+        return (
+            <div className="bg-white dark:bg-neutral-900 h-full ring-1 md:ring-0 ring-neutral-300 dark:ring-neutral-700">
+                <Editor
+                    language="cpp"
+                    value={content}
+                    onChange={(text, _event) => {
+                        if (id) setProblemImpl(id, text);
+                    }}
+                />
+            </div>
+        );
+    }
+
+    function BackToCompetition({ competitionId }: { competitionId?: number }): JSX.Element {
+        const { data, error } = useSWR<Competition>(
+            competitionId ? api_url(`/competitions/${competitionId}`) : null,
+            fetcher
+        );
+
+        if (!competitionId || error || !data) return <></>;
+
+        return (
+            <div className="border-neutral-300 dark:border-neutral-700 border-b p-2">
+                <h1 className="font-bold text-lg">{data.name}</h1>
+
+                <Link href={`/competitions/${competitionId}`}>
+                    <a className="text-blue-700 hover:underline dark:text-blue-500">
+                        ‹ Back to overview.
+                    </a>
+                </Link>
+            </div>
+        );
+    }
+
+    if (error) return <Error statusCode={404} />;
 
     return (
-      <div className="bg-white dark:bg-neutral-900 h-full ring-1 md:ring-0 ring-neutral-300 dark:ring-neutral-700">
-        <Editor
-          language="cpp"
-          value={content}
-          onChange={(text, _event) => {
-            if (id) setProblemImpl(id, text);
-          }}
-        />
-      </div>
+        <ProblemIDContext.Provider value={id}>
+            <ProblemContext.Provider value={data}>
+                <Head>
+                    <title>{data?.title}</title>
+                </Head>
+
+                <div className="grid grid-rows-[min-content_min-content_40vh_minmax(0,1fr)] md:grid-cols-[400px_minmax(0,1fr)] lg:grid-cols-[500px_minmax(0,1fr)] md:grid-rows-full-min h-full">
+                    <div className="md:border-r border-neutral-300 dark:border-neutral-700 row-span-2 flex flex-col border-b md:border-b-0">
+                        {data?.competition_id && <BackToCompetition competitionId={data?.competition_id} />}
+
+                        <TestContainer />
+
+                        <Tabbed
+                            titles={["Description", "History", "Leaderboard"]}
+                            className="h-full overflow-y-auto"
+                        >
+                            <Description />
+                            <SubmissionHistory />
+                            <ProblemLeaderboard />
+                        </Tabbed>
+                    </div>
+
+                    <ProblemEditorWrapper />
+                    <CodeRunner />
+                </div>
+            </ProblemContext.Provider>
+        </ProblemIDContext.Provider>
     );
-  }
-
-  function BackToCompetition({ competitionId }: { competitionId?: number }): JSX.Element {
-    const { data, error } = useSWR<Competition>(
-      competitionId ? api_url(`/competitions/${competitionId}`) : null,
-      fetcher
-    );
-
-    if (!competitionId || error || !data) return <></>;
-
-    return (
-      <div className="border-neutral-300 dark:border-neutral-700 border-b p-2">
-        <h1 className="font-bold text-lg">{data.name}</h1>
-
-        <Link href={`/competitions/${competitionId}`}>
-          <a className="text-blue-700 hover:underline dark:text-blue-500">
-            ‹ Back to overview.
-          </a>
-        </Link>
-      </div>
-    );
-  }
-
-  if (error) return <Error statusCode={404} />;
-
-  return (
-    <ProblemIDContext.Provider value={id}>
-      <ProblemContext.Provider value={data}>
-        <Head>
-          <title>{data?.title}</title>
-        </Head>
-
-        <div className="grid grid-rows-[min-content_min-content_40vh_minmax(0,1fr)] md:grid-cols-[400px_minmax(0,1fr)] lg:grid-cols-[500px_minmax(0,1fr)] md:grid-rows-full-min h-full">
-          <div className="md:border-r border-neutral-300 dark:border-neutral-700 row-span-2 flex flex-col border-b md:border-b-0">
-            {data?.competition_id && <BackToCompetition competitionId={data?.competition_id} />}
-
-            <TestContainer />
-
-            <Tabbed
-              titles={["Description", "History", "Leaderboard"]}
-              className="h-full overflow-y-auto"
-            >
-              <Description />
-              <SubmissionHistory />
-              <ProblemLeaderboard />
-            </Tabbed>
-          </div>
-
-          <ProblemEditorWrapper />
-          <CodeRunner />
-        </div>
-      </ProblemContext.Provider>
-    </ProblemIDContext.Provider>
-  );
 }
--- lilith/pages/submissions/[id].tsx
+++ lilith/pages/submissions/[id].tsx
@@ -14,155 +14,157 @@ import { useEffect, useRef } from "react";
 import SourceCodeBlock from "../../components/source-code";
 
 function InvalidateButton({ id }: { id?: number }): JSX.Element {
-  const submit = async () => {
-    await fetch(api_url(`/submissions/${id}/invalidate`), {
-      credentials: "include",
-    });
+    const submit = async () => {
+        await fetch(api_url(`/submissions/${id}/invalidate`), {
+            credentials: "include",
+        });
 
-    mutate(api_url(`/submissions/${id}`));
-  }
+        mutate(api_url(`/submissions/${id}`));
+    }
 
-  return (
-    <div className="w-full px-4 lg:px-0">
-      <button
-        onClick={submit}
-        className="w-full mt-4 rounded-full bg-red-600 hover:bg-red-700 px-4 py-2 text-red-50 transition-colors">
-        Invalidate
-      </button>
-    </div>
-  )
+    return (
+        <div className="w-full px-4 lg:px-0">
+            <button
+                onClick={submit}
+                className="w-full mt-4 rounded-full bg-red-600 hover:bg-red-700 px-4 py-2 text-red-50 transition-colors">
+                Invalidate
+            </button>
+        </div>
+    )
 }
 
 function ValidateButton({ id }: { id?: number }): JSX.Element {
-  const submit = async () => {
-    await fetch(api_url(`/submissions/${id}/validate`), {
-      credentials: "include",
-    });
+    const submit = async () => {
+        await fetch(api_url(`/submissions/${id}/validate`), {
+            credentials: "include",
+        });
 
-    mutate(api_url(`/submissions/${id}`));
-  }
+        mutate(api_url(`/submissions/${id}`));
+    }
 
-  return (
-    <div className="w-full px-4 lg:px-0">
-      <button
-        onClick={submit}
-        className="w-full mt-4 rounded-full bg-blue-600 hover:bg-blue-700 px-4 py-2 text-red-50 transition-colors">
-        Validate
-      </button>
-    </div>
-  )
+    return (
+        <div className="w-full px-4 lg:px-0">
+            <button
+                onClick={submit}
+                className="w-full mt-4 rounded-full bg-blue-600 hover:bg-blue-700 px-4 py-2 text-red-50 transition-colors">
+                Validate
+            </button>
+        </div>
+    )
 }
 
 type UserInfoProps = {
-  id: number
+    id: number
 };
 
 function UserInfo({ id }: UserInfoProps): JSX.Element {
-  const { data, error } = useSWR<User>(
-    id ? api_url(`/user/id/${id}`) : null,
-    fetcher
-  );
+    const { data, error } = useSWR<User>(
+        id ? api_url(`/user/id/${id}`) : null,
+        fetcher
+    );
 
-  if (error)
-    return <>Error</>;
+    if (error)
+        return <>Error</>;
 
-  if (!data)
-    return <div>loading</div>;
+    if (!data)
+        return <div>loading</div>;
 
-  return (
-    <Link href={`/user/${data.username}`}>
-      {data.name}
-    </Link>
-  );
+    return (
+        <Link href={`/user/${data.username}`}>
+            {data.name}
+        </Link>
+    );
 }
 
 function ProblemTitle({ id }: { id: number }): JSX.Element {
-  const { data } = useSWR<Problem>(
-    api_url(`/problems/${id}`),
-    fetcher
-  );
+    const { data } = useSWR<Problem>(
+        api_url(`/problems/${id}`),
+        fetcher
+    );
 
-  if (!data) return (
-    <div className="animate-pulse bg-neutral-300 dark:bg-neutral-700 h-9 w-[75%] rounded-md"></div>
-  );
+    if (!data) return (
+        <div className="animate-pulse bg-neutral-300 dark:bg-neutral-700 h-9 w-[75%] rounded-md"></div>
+    );
 
-  return (
-    <h1 className="text-3xl font-extrabold">{data.title}</h1>
-  );
+    return (
+        <h1 className="text-3xl font-extrabold">{data.title}</h1>
+    );
 }
 
 const SubmissionPage: NextPage = () => {
-  const router = useRouter();
-  const id = router.isReady ? parseInt(router.query.id as string) : undefined;
-  const setProblemImpl = useStore((state) => state.setProblemImpl);
+    const router = useRouter();
+    const id = router.isReady ? parseInt(router.query.id as string) : undefined;
+    const setProblemImpl = useStore((state) => state.setProblemImpl);
 
-  const { data: submission, error } = useSWR<Submission>(
-    id ? api_url(`/submissions/${id}`) : null,
-    fetcher
-  );
+    const { data: submission, error } = useSWR<Submission>(
+        id ? api_url(`/submissions/${id}`) : null,
+        fetcher
+    );
 
-  const { data: user, error: _error } = useSWR<User>(
-    api_url("/user/me"),
-    fetcher, {
-    shouldRetryOnError: false,
-  });
+    const { data: user, error: _error } = useSWR<User>(
+        api_url("/user/me"),
+        fetcher, {
+        shouldRetryOnError: false,
+    });
 
-  if (error)
-    return <Error statusCode={404} />;
+    if (error)
+        return <Error statusCode={404} />;
 
-  if (!submission) {
-    return <Navbar />;
-  }
+    if (!submission) {
+        return <Navbar />;
+    }
 
-  return <>
-    <Navbar />
+    return <>
+        <Navbar />
 
-    <Head>
-      <title>Submission {id}</title>
-    </Head>
+        <Head>
+            <title>Submission {id}</title>
+        </Head>
 
-    <div className="grid grid-cols-[minmax(0,1fr)] grid-rows-min-full gap-4 lg:grid-cols-[minmax(0,1fr)_320px] mt-4 md:flex-row max-w-screen-lg md:mx-auto md:mt-8">
-      <div className="p-4 border-neutral-300 bg-white dark:bg-black dark:border-neutral-700 border-y lg:border lg:rounded-md flex flex-col gap-4 max-w row-start-2 lg:row-start-1">
-        <div className="flex flex-col gap-1">
-          <div className="flex flex-col">
-            <ProblemTitle id={submission.problem_id} />
-          </div>
-          <span className="text-sm text-neutral-500">
-            <UserInfo id={submission.user_id} />
-            {" • "}
-            {timeFormat(submission.time + 'Z')}
-          </span>
+        <div className="grid grid-cols-[minmax(0,1fr)] grid-rows-min-full gap-4 lg:grid-cols-[minmax(0,1fr)_320px] mt-4 md:flex-row max-w-screen-lg md:mx-auto md:mt-8">
+            <div className="p-4 border-neutral-300 bg-white dark:bg-black dark:border-neutral-700 border-y lg:border lg:rounded-md flex flex-col gap-4 max-w row-start-2 lg:row-start-1">
+                <div className="flex flex-col gap-1">
+                    <div className="flex flex-col">
+                        <ProblemTitle id={submission.problem_id} />
+                    </div>
+                    <span className="text-sm text-neutral-500">
+                        <UserInfo id={submission.user_id} />
+                        {" • "}
+                        {timeFormat(submission.time + 'Z')}
+                    </span>
+                </div>
+
+                <SourceCodeBlock text={submission.code} />
+
+                <div className="mt-auto p-4 rounded-md bg-yellow-300 border-yellow-500 border text-yellow-900">
+                    <h1 className="font-bold text-xl mb-2">{"Warning"}</h1>
+                    <p>{"You will lose your current progress."}</p>
+                </div>
+
+                <a href={`/problems/${submission.problem_id}`}
+                    onClick={event => {
+                        event.preventDefault();
+                        setProblemImpl(submission.problem_id, submission.code);
+                        router.push(`/problems/${submission.problem_id}`);
+                    }}
+                    className="rounded-full p-2 bg-blue-600 hover:bg-blue-500 text-blue-50 transition-colors text-center">
+                    {"View in editor"}
+                </a>
+            </div>
+            <div className="col-row-1 relative">
+                <div className="sticky top-20">
+                    <div className="border-y lg:border lg:rounded-md border-neutral-300 dark:border-neutral-700 overflow-hidden">
+                        <SubmissionFeedback inProblemView={false} {...submission} />
+                    </div>
+
+                    {(user && (user.auth == "ADMIN" || user.auth == "OFFICER")) && <>
+                        <ValidateButton id={id} />
+                        <InvalidateButton id={id} />
+                    </>}
+                </div>
+            </div>
         </div>
-
-        <SourceCodeBlock text={submission.code} />
-
-        <div className="mt-auto p-4 rounded-md bg-yellow-300 border-yellow-500 border text-yellow-900">
-          <h1 className="font-bold text-xl mb-2">{"Warning"}</h1>
-          <p>{"You will lose your current progress."}</p>
-        </div>
-
-        <a href={`/problems/${submission.problem_id}`}
-          onClick={event => {
-            event.preventDefault();
-            setProblemImpl(submission.problem_id, submission.code);
-            router.push(`/problems/${submission.problem_id}`);
-          }}
-          className="rounded-full p-2 bg-blue-600 hover:bg-blue-500 text-blue-50 transition-colors text-center">
-          {"View in editor"}
-        </a>
-      </div>
-      <div className="col-row-1">
-        <div className="border-y lg:border lg:rounded-md border-neutral-300 dark:border-neutral-700 overflow-hidden">
-          <SubmissionFeedback inProblemView={false} {...submission} />
-        </div>
-
-        {(user && (user.auth == "ADMIN" || user.auth == "OFFICER")) && <>
-          <ValidateButton id={id} />
-          <InvalidateButton id={id} />
-        </>}
-      </div>
-    </div>
-  </>;
+    </>;
 };
 
 export default SubmissionPage;
