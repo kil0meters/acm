61d4d2fc2c9ee15ca74aafbfd54e7049887db846
kilometers <kilo@meters.sh>
2023-04-25 17:02:26 -0700

    Add example problem back to index

--- lilith/pages/index.tsx
+++ lilith/pages/index.tsx
@@ -2,27 +2,35 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
+import ProblemView from "../components/problem";
 // import ProblemView from "../components/problem";
 import { CompetitionGrid } from "./competitions";
 
 const Home: NextPage = () => {
-  return (
-    <div className="overflow-x-hidden flex flex-col gap-4 min-h-screen justify-center items-center">
-      <Head>
-        <title>Chico ACM</title>
-      </Head>
+    return (
+        <div className="overflow-x-hidden flex flex-col gap-4 min-h-screen">
+            <Head>
+                <title>Chico ACM</title>
+            </Head>
 
-      <Navbar />
+            <Navbar />
 
-      <h2 className="py-4 text-center text-6xl drop-shadow-md font-extrabold text-transparent bg-clip-text bg-gradient-to-b from-neutral-600 to-neutral-900 dark:from-neutral-50 dark:to-neutral-400">
-        {"Chico ACM"}
-      </h2>
+            <h1 className="py-4 text-6xl drop-shadow-md text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-b from-neutral-600 to-neutral-900 dark:from-neutral-50 dark:to-neutral-400">
+                Chico ACM
+            </h1>
 
-      <CompetitionGrid />
+            <div className="md:container w-full mx-auto">
+                <h2 className="font-bold text-xl mx-2 md:mx-0">Local Competitions</h2>
+                <CompetitionGrid />
+            </div>
 
-      <Footer />
-    </div>
-  );
+            <div className="md:container md:h-[80vh] md:shadow md:rounded border-neutral-300 mx-auto border-y md:border-x w-full overflow-auto">
+                <ProblemView id={1} />
+            </div>
+
+            <Footer />
+        </div>
+    );
 };
 
 export default Home;
