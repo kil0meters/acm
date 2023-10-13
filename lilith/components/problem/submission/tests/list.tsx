09b398df23fbceb8eed0b7a30890c42bd941e103
kilometers <kilo@meters.sh>
2023-10-13 12:24:41 -0700

    Improve string list readability

--- lilith/components/problem/submission/tests/list.tsx
+++ lilith/components/problem/submission/tests/list.tsx
@@ -1,18 +1,31 @@
 import { ReactNode } from "react";
 
 export function ListDisplay<T extends ReactNode>({ dataType, data }: { dataType: string, data: T[] }): JSX.Element {
-    return (
-        <div>
-            <span className="text-sm">{dataType} list:</span>
-            <div
-                className="rounded border w-min bg-blue-50 border-blue-200 max-w-full max-h-[30vh] overflow-auto flex dark:border-slate-700 dark:bg-slate-800">
-                {data.length == 0
-                    ? <span className="whitespace-nowrap px-4 py-2">[ EMPTY LIST ]</span>
-                    : data.map((element, i) => <span key={i} className="text-center px-4 py-2 border-l first:border-0 border-blue-200 dark:border-slate-700">{element}</span>)
-                }
+    if (dataType === "String") {
+        return (
+            <div>
+                <span className="text-sm">{dataType} list:</span>
+                <div className={`rounded border flex-col whitespace-nowrap w-min bg-blue-50 border-blue-200 max-w-full max-h-[30vh] overflow-auto flex dark:border-slate-700 dark:bg-slate-800`}>
+                    {data.length == 0
+                        ? <span className="whitespace-nowrap px-4 py-2">[ EMPTY LIST ]</span>
+                        : data.map((element, i) => <span key={i} className="text-center px-4 py-2 border-t first:border-0 border-blue-200 dark:border-slate-700">{element}</span>)
+                    }
+                </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return (
+            <div>
+                <span className="text-sm">{dataType} list:</span>
+                <div className={`rounded border w-min bg-blue-50 border-blue-200 max-w-full max-h-[30vh] overflow-auto flex dark:border-slate-700 dark:bg-slate-800`}>
+                    {data.length == 0
+                        ? <span className="whitespace-nowrap px-4 py-2">[ EMPTY LIST ]</span>
+                        : data.map((element, i) => <span key={i} className="text-center px-4 py-2 border-l first:border-0 border-blue-200 dark:border-slate-700">{element}</span>)
+                    }
+                </div>
+            </div>
+        );
+    }
 }
 
 export function ListDiffDisplay<T extends ReactNode>({ dataType, output, expected }: { dataType: string, output: T[], expected: T[] }): JSX.Element {
